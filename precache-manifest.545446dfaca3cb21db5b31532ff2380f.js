ec7f609c0c610ba7f151728e5d25271826025861
Majid <nastaran.samui@gmail.com>
2020-09-06 17:13:08 +0700

    Updates

---
+++ precache-manifest.545446dfaca3cb21db5b31532ff2380f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a294f99098058f1538141f7f636334",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86523eb88fe0f7097f16",
+    "url": "/static/css/main.bfa04731.chunk.css"
+  },
+  {
+    "revision": "98a68b67b921b5b7b0ca",
+    "url": "/static/js/2.11cf55e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.11cf55e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86523eb88fe0f7097f16",
+    "url": "/static/js/main.07d894d1.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  },
+  {
+    "revision": "778f6505ea2b48c01f6b3db041347a5c",
+    "url": "/static/media/1.778f6505.png"
+  }
+]);
