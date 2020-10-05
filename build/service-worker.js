ebaa44ba76cbb5dff84e3114ac082cb9cd58786b
Majid <nastaran.samui@gmail.com>
2020-10-05 09:58:14 +0700

    Links

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5bdcf7d98ac9faeac1ac4bb2ac870cbc.js"
+  "/precache-manifest.26301e9de52725bd35c738d47a7cbd07.js"
 );
 
 self.addEventListener('message', (event) => {
